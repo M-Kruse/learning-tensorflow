efd07a65b99d7015cd6280d30c1f3f348c950468
M-Kruse <matthewdkruse@comcast.net>
2020-01-06 19:27:00 -0800

    new stuff

--- LearningTensorFlow.py
+++ LearningTensorFlow.py
@@ -209,11 +209,16 @@ class LearningTensorFlow(object):
                     print("Running Dot After Op: {0}".format(dot))
         return dot
 
+    def vectorize_sequences(self, sequences, dimension=10000):
+        results = np.zeros((len(sequences), dimension))
+        for i, sequence in enumerate(sequences):
+            results[i, sequence] = 1.
+        return results
 
 if __name__ == '__main__':
 
     ltf = LearningTensorFlow()
-
+    
     print("TensorFlow Device Testing...\n")
 
     devs = ltf.get_device_list()
@@ -242,24 +247,23 @@ if __name__ == '__main__':
     # ltf.example_vector_dot(v1, v2)
 
     #Example vector matrix dot product
-    v3 = np.array([1, 2, 3])
-    m1 = np.array([
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 6, 9]
-                ])
+    # v3 = np.array([1, 2, 3])
+    # m1 = np.array([
+    #                 [1, 4, 7],
+    #                 [2, 5, 8],
+    #                 [3, 6, 9]
+    #             ])
 
     #ltf.example_matrix_vector_dot(m1, v3)
 
-    m2 = np.array([
-                        [1, 4, 7],
-                        [2, 5, 8],
-                        [3, 6, 9]
-                    ])
+    # m2 = np.array([
+    #                     [1, 4, 7],
+    #                     [2, 5, 8],
+    #                     [3, 6, 9]
+    #                 ])
 
 
-    print(ltf.example_matrix_dot(m1, m2))
-
+    #print(ltf.example_matrix_dot(m1, m2))
 
 
     # fashion_mnist = keras.datasets.fashion_mnist
@@ -316,6 +320,169 @@ if __name__ == '__main__':
     #     outputs={t.name:t for t in model.outputs})
 
     # print('\nSaved model:')
+
+    # from keras.datasets import imdb
+    # (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+    
+    # x_train = ltf.vectorize_sequences(train_data)
+    # x_test = ltf.vectorize_sequences(test_data)
+
+    # y_train = np.asarray(train_labels).astype('float32')
+    # y_test = np.asarray(test_labels).astype('float32')
+
+    # x_train = ltf.vectorize_sequences(train_data)
+    # x_test = ltf.vectorize_sequences(test_data)
+
+    # print(len(train_data))
+    # print(len(test_data))
+    # print(train_data[10])
+
+    from keras.datasets import reuters
+    word_index = reuters.get_word_index()
+    (train_data, train_labels), (test_data, test_labels) = reuters.load_data(
+        num_words=10000)
+    
+    reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+    decoded_newswire = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+    x_train = ltf.vectorize_sequences(train_data)
+    x_test = ltf.vectorize_sequences(test_data)
+    
+    def to_one_hot(labels, dimension=46):
+        results = np.zeros((len(labels), dimension))
+        for i, label in enumerate(labels):
+            results[i, label] = 1.
+        return results
+
+    # one_hot_train_labels = to_one_hot(train_labels)
+    # one_hot_test_labels = to_one_hot(test_labels)
+        
+    from keras.utils.np_utils import to_categorical
+    one_hot_train_labels = to_categorical(train_labels)
+    one_hot_test_labels = to_categorical(test_labels)
+
+    from keras import models
+    from keras import layers
+    # model = models.Sequential()
+    # model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+    # model.add(layers.Dense(64, activation='relu'))
+    # model.add(layers.Dense(46, activation='softmax'))
+
+    # model.compile(optimizer='rmsprop',
+    #     loss='categorical_crossentropy',
+    #     metrics=['accuracy'])
+
+
+    x_val = x_train[:1000]
+    partial_x_train = x_train[1000:]
+
+    y_val = one_hot_train_labels[:1000]
+    partial_y_train = one_hot_train_labels[1000:]
+
+    # history = model.fit(partial_x_train,
+    #     partial_y_train,
+    #     epochs=20,
+    #     batch_size=512,
+    #     validation_data=(x_val, y_val))
+
+    # import matplotlib.pyplot as plt
+    # loss = history.history['loss']
+    # val_loss = history.history['val_loss']
+    # epochs = range(1, len(loss) + 1)
+    # plt.plot(epochs, loss, 'bo', label='Training loss')
+    # plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    # plt.title('Training and validation loss')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
+
+    # plt.clf()
+    # acc = history.history['acc']
+    # val_acc = history.history['val_acc']
+    # plt.plot(epochs, acc, 'bo', label='Training acc')
+    # plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    # plt.title('Training and validation accuracy')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
+
+
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(46, activation='softmax'))
+    model.compile(optimizer='rmsprop',
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+    model.fit(partial_x_train,
+        partial_y_train,
+        epochs=9,
+        batch_size=512,
+        validation_data=(x_val, y_val))
+    results = model.evaluate(x_test, one_hot_test_labels)
+
+    predictions = model.predict(x_test)
+
+    # from keras import models
+    # from keras import layers
+    # model = models.Sequential()
+    # model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    # model.add(layers.Dense(16, activation='relu'))
+    # model.add(layers.Dense(1, activation='sigmoid'))
+
+    # x_val = x_train[:10000]
+    # partial_x_train = x_train[10000:]
+    # y_val = y_train[:10000]
+    # partial_y_train = y_train[10000:]
+
+
+    # model.compile(optimizer='rmsprop',
+    #     loss='binary_crossentropy',
+    #     metrics=['acc'])
+    # history = model.fit(partial_x_train,
+    #     partial_y_train,
+    #     epochs=20,
+    #     batch_size=512,
+    #     validation_data=(x_val, y_val))
+
+    # import matplotlib.pyplot as plt
+    # history_dict = history.history
+    # loss_values = history_dict['loss']
+    # val_loss_values = history_dict['val_loss']
+    # acc = history_dict['acc']
+    # val_acc = history_dict['val_acc']
+    # epochs = range(1, len(acc) + 1)
+    # plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    # plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+    # plt.title('Training and validation loss')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
+    # acc_values = history_dict['acc']
+    # val_acc_values = history_dict['val_acc']
+    # plt.plot(epochs, acc, 'bo', label='Training acc')
+    # plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    # plt.title('Training and validation accuracy')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
+
+    # model = models.Sequential()
+    # model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    # model.add(layers.Dense(16, activation='relu'))
+    # model.add(layers.Dense(1, activation='sigmoid'))
+    # model.compile(optimizer='rmsprop',
+    #     loss='binary_crossentropy',
+    #     metrics=['accuracy'])
+    # model.fit(x_train, y_train, epochs=4, batch_size=512)
+    # results = model.evaluate(x_test, y_test)
+
+    # print(model.predict(x_test))
+
 
 
 # devel@devel:~/Code/learning-tensorflow$ ~/.local/bin/saved_model_cli show --dir ~/Code/my_model_zoo/tensorflow/serve_test/1/ --all
